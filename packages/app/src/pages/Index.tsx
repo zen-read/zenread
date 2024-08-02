b1182ebcd530bf0114cdce7affa5f0b9323540b4
Mike Noired <unkstd@mail.ru>
2024-08-02 17:41:12 +0300

    Add template posts w/ data

--- packages/app/src/pages/Index.tsx
+++ packages/app/src/pages/Index.tsx
@@ -1,7 +1,38 @@
+import { PostCard } from "@zenread/ui";
 import BodyWrapper from "../components/BodyWrapper.js";
 
+const templateData = [
+  {
+    backgroundImage:
+      "https://i1.sndcdn.com/artworks-oGvaMt0dsBWPL7WZ-qgbAMA-t500x500.jpg",
+    header: "Maxwell",
+    date: new Date(),
+    originLogo:
+      "https://m.media-amazon.com/images/I/31+8EYj8ZdL._UXNaN_FMjpg_QL85_.jpg",
+    loaded: true,
+  },
+  {
+    backgroundImage:
+      "https://i1.sndcdn.com/artworks-oGvaMt0dsBWPL7WZ-qgbAMA-t500x500.jpg",
+    header: "Maxwell",
+    date: new Date(),
+    originLogo:
+      "https://m.media-amazon.com/images/I/31+8EYj8ZdL._UXNaN_FMjpg_QL85_.jpg",
+    loaded: true,
+  },
+];
+
 const Index = () => {
-  return <BodyWrapper fullWidth={false}>Index</BodyWrapper>;
+  return (
+    <BodyWrapper fullWidth={false}>
+      <h1 className="text-5xl font-bold w-full pb-3 border-b">Latest</h1>
+      <div className="mt-7 grid grid-cols-2 gap-[18px]">
+        {templateData.map((data, index) => (
+          <PostCard key={index} {...data} />
+        ))}
+      </div>
+    </BodyWrapper>
+  );
 };
 
 export default Index;
