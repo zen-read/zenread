eb5847aa3fa9bcb1bbf2b34beaea8a203672de6e
Mike Noired <unkstd@mail.ru>
2024-08-03 17:26:57 +0300

    Add className param to BodyWrapper

--- packages/app/src/components/BodyWrapper.tsx
+++ packages/app/src/components/BodyWrapper.tsx
@@ -4,13 +4,14 @@ import Header from "../components/Header.js";
 interface BodyWrapperProps {
   children: React.ReactNode;
   fullWidth?: boolean;
+  className?: string;
 }
 
-const BodyWrapper = ({ children, fullWidth }: BodyWrapperProps) => {
+const BodyWrapper = ({ children, fullWidth, className }: BodyWrapperProps) => {
   return (
     <div className={cn("w-full", !fullWidth && "flex justify-center")}>
       <Header />
-      <div className={cn("mt-[80px]", !fullWidth && "w-[1025px]")}>
+      <div className={cn("mt-[80px]", !fullWidth && "w-[1025px]", className)}>
         {children}
       </div>
     </div>
