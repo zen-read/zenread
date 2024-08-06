af7c08f9fda4dfc705f02c4e88c6729f4fa9376c
Mike Noired <unkstd@mail.ru>
2024-08-06 16:07:38 +0300

    Revert "feat(add empty store)"
    This reverts commit c173337b3d96d7c3018fe20246d34ec2a68931ed.
    
    # Conflicts:
    #	packages/app/src/main.tsx
    #	packages/app/src/store.ts

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./pages/Index.js";
 import OriginPage from "./pages/OriginPage.js";
 import PostPage from "./pages/PostPage.js";
 import ReadList from "./pages/ReadList.js";
-import { settingsOpenedStore } from "./store.js";
 import "./styles.css";
 import { checkConfig } from "./utils/checkConfig.js";
 
@@ -33,8 +31,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={settingsOpenedStore}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
