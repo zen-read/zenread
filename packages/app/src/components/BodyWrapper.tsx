d2ba1a54fe16e12add19e0b89744c3ffbcf49f73
Mike Noired <unkstd@mail.ru>
2024-08-06 16:13:41 +0300

    fix(redux): remove other breadcrumbs

--- packages/app/src/components/BodyWrapper.tsx
+++ packages/app/src/components/BodyWrapper.tsx
@@ -1,7 +1,5 @@
 import { cn } from "@zenread/shared";
-import { useEffect, useReducer } from "react";
 import Header from "../components/Header.js";
-import { settingsOpenedStore } from "../store.js";
 import SettingsOverlay from "./SettingsOverlay.js";
 
 interface BodyWrapperProps {
@@ -11,18 +9,9 @@ interface BodyWrapperProps {
 }
 
 const BodyWrapper = ({ children, fullWidth, className }: BodyWrapperProps) => {
-  const [, forceUpdate] = useReducer((x) => x + 1, 0);
-  useEffect(() => {
-    const unsubscribe = settingsOpenedStore.subscribe(() => {
-      forceUpdate();
-    });
-
-    return unsubscribe;
-  }, []);
-
   return (
     <div className={cn("w-full", !fullWidth && "flex justify-center")}>
-      {settingsOpenedStore.getState().opened && <SettingsOverlay />}
+      <SettingsOverlay />
       <Header />
       <div className={cn("mt-[80px]", !fullWidth && "w-[1025px]", className)}>
         {children}
