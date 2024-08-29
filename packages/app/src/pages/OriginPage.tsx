d9908e7e618ee65412d06eb8594101ea2e9be3a4
Mike Noired <unkstd@mail.ru>
2024-08-30 00:25:36 +0300

    feat(origin page): add saving origin to user store by click

--- packages/app/src/pages/OriginPage.tsx
+++ packages/app/src/pages/OriginPage.tsx
@@ -1,11 +1,13 @@
 import { OriginBlock, PostCard } from "@zenread/ui";
+import { useCallback, useEffect } from "react";
 import BodyWrapper from "../components/BodyWrapper.js";
+import { useUserStore } from "../store/useUserStore.js";
+import { updateUserStore } from "../utils/updateUserStore.js";
 
 const originInfo = {
   originLogo:
     "https://m.media-amazon.com/images/I/31+8EYj8ZdL._UXNaN_FMjpg_QL85_.jpg",
   originName: "Maxwell.com",
-  isSaved: false,
   description:
     "Maxwell the Cat, also known as Spinning Cat is a black-and-white tuxedo cat who achieved virality online in October and November 2022 after her model was first added to Garry's Mod and later used as source material for videos in which it spins around as various songs play, similar to the Horizontally Spinning Rat memes. The cat is also known as Dingus the Cat and Big Boobs, and its real name is Jess.",
   links: [
@@ -43,9 +45,38 @@ const templateArticles = [
 ];
 
 const OriginPage = () => {
+  const { userStore, setUserStore } = useUserStore();
+  const isOriginSaved = userStore.origins.some(
+    (origin) => origin === originInfo.originName,
+  );
+
+  const saveHandler = useCallback(async () => {
+    !isOriginSaved
+      ? setUserStore({
+          tags: userStore.tags,
+          origins: [...userStore.origins, originInfo.originName],
+        })
+      : setUserStore({
+          tags: userStore.tags,
+          origins: userStore.origins.filter(
+            (origin) => origin !== originInfo.originName,
+          ),
+        });
+  }, [userStore, originInfo.originName, isOriginSaved]);
+
+
+  useEffect(() => {
+    updateUserStore(userStore);
+  }, [userStore]);
+
   return (
     <BodyWrapper fullWidth={false}>
-      <OriginBlock className="mt-11" {...originInfo} />
+      <OriginBlock
+        isSaved={isOriginSaved}
+        saveHandler={saveHandler}
+        className="mt-11"
+        {...originInfo}
+      />
       <h1 className="text-5xl font-bold w-full pb-3 border-b mt-7">
         Latest articles from {originInfo.originName}
       </h1>
