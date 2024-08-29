58fa2cff48ee598987841bdc4e66aac4c0043182
Mike Noired <unkstd@mail.ru>
2024-08-29 15:51:38 +0300

    feat(ui): get values from ref

--- packages/ui/src/Slider/index.tsx
+++ packages/ui/src/Slider/index.tsx
@@ -1,3 +1,5 @@
+import { useState, useImperativeHandle, RefObject, ChangeEvent } from "react";
+
 interface SliderProps {
   /** Optional name of slider */
   label?: string;
@@ -7,10 +9,17 @@ interface SliderProps {
   min?: number;
   /** Max value */
   max?: number;
+  /** Slider ref */
+  ref: RefObject<{ getValue: () => number | undefined }>;
 }
 
 /** Slider control */
-export const Slider = ({ label, value, min, max }: SliderProps) => {
+export const Slider = ({ label, value, min, max, ref }: SliderProps) => {
+  const [sliderValue, setSliderValue] = useState(value);
+  const getValue = () => sliderValue;
+  useImperativeHandle(ref, () => ({ getValue }));
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => setSliderValue(+event.target.value);
+
   return (
     <label htmlFor="slider" className="flex items-center gap-4">
       <span className="font-bold">{label}</span>
@@ -20,7 +29,8 @@ export const Slider = ({ label, value, min, max }: SliderProps) => {
         name="slider"
         min={min ? min : 0}
         max={max ? max : 100}
-        defaultValue={value}
+        value={sliderValue}
+        onChange={handleChange}
       />
     </label>
   );
--- packages/ui/src/Toggle/index.tsx
+++ packages/ui/src/Toggle/index.tsx
@@ -1,17 +1,26 @@
 import { cn } from "@zenread/shared";
 import { useState } from "react";
+import { RefObject, useImperativeHandle } from "react";
 
 interface ToggleProps {
   /** Is that toggle active? */
   active: boolean;
+  ref: RefObject<{ getValue: () => boolean | undefined }>
 }
 
 /** Toggle button */
-export const Toggle = ({ active }: ToggleProps) => {
+export const Toggle = ({ active, ref }: ToggleProps) => {
   const [isActive, setIsActive] = useState(active);
+
+  const getValue = () => isActive;
+
+  useImperativeHandle(ref, () => ({ getValue }));
+
+  const handleChange = () => setIsActive(!isActive);
+
   return (
     <div
-      onClick={() => setIsActive(!isActive)}
+      onClick={() => handleChange()}
       className={cn(
         "transition-color flex h-7 w-11 cursor-pointer items-center rounded-full duration-150 ease-in-out",
         isActive ? "bg-secondary" : "border border-secondary bg-primary",
