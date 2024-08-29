a34653149d0e47ac367024656de8cce73ede751e
Mike Noired <unkstd@mail.ru>
2024-08-29 17:12:11 +0300

    feat(settings): make modal more responsible

--- packages/app/src/components/SettingsOverlay/index.tsx
+++ packages/app/src/components/SettingsOverlay/index.tsx
@@ -1,20 +1,22 @@
-import { Categories } from "./Categories.js";
-import { Heading } from "./Heading.js";
 import { useParams } from "react-router-dom";
+import { Categories } from "./Categories.js";
 import { Content } from "./Content.js";
+import { Heading } from "./Heading.js";
 
 const SettingsOverlay = () => {
   const { id } = useParams();
 
   return (
     <div className="z-50 fixed w-screen h-screen bg-black-25% flex items-center justify-center">
-      <div className="w-[1024px] h-[720px] bg-primary rounded-md grid grid-cols-[1fr_3fr] border border-secondary-25%">
-        <div className="border-r border-secondary-25%">
-          <Heading />
-          <Categories />
-        </div>
-        <div className="flex flex-col w-full h-full p-6">
-          {id && <Content link={id} />}
+      <div className="lg:w-[1024px] w-full hmd:h-[768px] h-full p-6 flex items-center justify-center">
+        <div className="w-full h-full bg-primary rounded-md grid grid-cols-[1fr_3fr] border border-secondary-25%">
+          <div className="border-r border-secondary-25%">
+            <Heading />
+            <Categories />
+          </div>
+          <div className="flex flex-col w-full h-full p-6">
+            {id && <Content link={id} />}
+          </div>
         </div>
       </div>
     </div>
