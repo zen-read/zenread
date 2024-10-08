7a839ce79edff071e9e8895bd4c3ecde35c3781f
Mike Noired <unkstd@mail.ru>
2024-10-08 17:45:22 +0300

    fix(index): grid pos

--- packages/app/src/pages/Index.tsx
+++ packages/app/src/pages/Index.tsx
@@ -30,7 +30,7 @@ const Index = () => {
   return (
     <BodyWrapper fullWidth={false}>
       <h1 className="text-5xl font-bold w-full pb-3 border-b">Latest</h1>
-      <div className="mt-5 grid grid-cols-2 gap-[18px]">
+      <div className="mt-5 grid sm:grid-cols-2 grid-cols-1 gap-[18px]">
         {templateData.map((data, index) => (
           <PostCard key={index} {...data} />
         ))}
